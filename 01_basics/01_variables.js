9d994bf5b8ddc84351cb589381d9a9435c548897
mamta1423 <mamta.parate31@gmail.com>
2025-03-06 11:40:02 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountEmail = "mamta@m.com"
+var accountCity = "Raipur"
+accountPassword = "33333"
+let accountState;
+
+
+accountEmail="mamta@gmail.com"
+accountCity="Bhilai"
+accountPassword="44444"
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+console.log(accountState)
+console.table(accountId, accountEmail, accountPassword, accountCity, accountState)
